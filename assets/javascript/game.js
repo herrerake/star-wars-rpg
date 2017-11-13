0f83af90177592ab4b376557b05ef4b0b978852f
Kevin Herrera <1herrerake@gmail.com>
2017-11-13 13:08:27 -0500

    loaded jquery. created character objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  // GLOBAL VARIABLES
+
+  // Creating an object to hold our characters.
+  var characters = {
+    "Luke Skywalker": {
+      name: "Luke Skywalker",
+      health: 140,
+      attack: 30,
+      imageUrl: "assets/images/luke.jpg",
+      counterAttack: 15
+    },
+    "Princess Leia": {
+      name: "Princess Leia",
+      health: 200,
+      attack: 10,
+      imageUrl: "assets/images/leia.jpg",
+      counterAttack: 20
+    },
+    "Yoda": {
+      name: "Yoda",
+      health: 160,
+      attack: 20,
+      counterAttack: 40
+    },
+    "Darth Vader": {
+      name: "Darth Vader",
+      health: 150,
+      attack: 20,
+      counterAttack: 40
+    }
+  }
+});
